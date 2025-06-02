e6f2bd4181da75941f75e98e396eed6e6f5b0cc4
Sebastian Osterlund <sebastian.osterlund@intel.com>
2025-06-02 21:56:02 +0200

    pts/leveldb-1.1.0: Pull in third_party libs and use cmake to build
    leveldb doesn't build against gmock in the ubuntu repos. Pull in the
    third-party libs from the leveldb repo (release closest to the pinned
    commit).
    
    Also, this uses cmake --build, rather than make to build the target.

--- pts/leveldb-1.1.0/install.sh
+++ pts/leveldb-1.1.0/install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 tar -xf leveldb-1.23.tar.gz
 cd leveldb-1.23
+tar xf ../googletest-1.11.0.tar.gz -C third_party/googletest --strip-components=1
+tar xf ../benchmark-1.8.0.tar.gz -C third_party/benchmark --strip-components=1
 sed -i '4 a #include <ctime>' ./benchmarks/db_bench_sqlite3.cc
-sed -i '299d' CMakeLists.txt
-sed -i '304d' CMakeLists.txt
 mkdir build
 cd build
-cmake  -DCMAKE_BUILD_TYPE=Release -DLEVELDB_BUILD_TESTS=OFF ..
-make -j $NUM_CPU_THREADS
+cmake  -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
 echo $? > ~/install-exit-status
 cd ~
 echo "#!/bin/sh
